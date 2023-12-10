f9961e5a683761678028306a946dff6dfa59a720
red-apple38 <alibozkoyun38@gmail.com>
2023-12-10 18:10:21 +0100

    Logs completed

--- logs.py
+++ logs.py
@@ -2,9 +2,11 @@ import logging
 
 def writelogs(error):
     logging.basicConfig(
-        level=logging.Error,
+        level=logging.ERROR,
         format='%(asctime)s %(levelname)s %(message)s',
         datefmt="%Y-%m-%d %H:%M:",
         filename="logs-tracer.log"
         )
     logging.error(error)
+
+
